27950b974d52c7cc6ca680347d3ff9295bb31791
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2023-03-12 13:52:36 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Number0_1000
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 2;
+            int end = 1000;
+            var task = Task.Run(() =>
+            {
+                int count = 0;
+                for (int i = start; i <= end; i++)
+                {
+                    bool isPrime = true;
+                    for (int j = 2; j <= Math.Sqrt(i); j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            });
+            int primeCount = task.Result;
+            Console.WriteLine("Кількість простих чисел між {0} і {1}: {2}", start, end, primeCount);
+            task.Wait();
+        }
+    }
+}
